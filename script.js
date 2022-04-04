50cc0b252b141682f55e1b6b616a547176f91f9b
IshitaKanwar04 <kanwar.ishita04@gmail.com>
2022-04-04 23:08:36 +0530

    added styling

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log("start")
+// variables
+
+let songIndex=0;
+let audioElement = new Audio('Golden.mp3')
+
+let songs=[
+
+    {songName:"Golden",filePath:"music/Golden.mp3",coverPath:"images/cover1,jpg"},
+    {songName:"Golden",filePath:"music/Golden.mp3",coverPath:"images/cover1,jpg"},
+    {songName:"Golden",filePath:"music/Golden.mp3",coverPath:"images/cover1,jpg"},
+    {songName:"Golden",filePath:"music/Golden.mp3",coverPath:"images/cover1,jpg"},
+    {songName:"Golden",filePath:"music/Golden.mp3",coverPath:"images/cover1,jpg"},
+    {songName:"Golden",filePath:"music/Golden.mp3",coverPath:"images/cover1,jpg"}
+
+]
+
